50dc222d5c1bd35e4a1c96ed01c0210adce24b12
Savio Fernandes <savioferns321@gmail.com>
2017-04-16 16:46:59 -0700

    Modifying main file

--- src/main/java/com/example/fencedetails/GeoFenceDetailServiceApplication.java
+++ src/main/java/com/example/fencedetails/GeoFenceDetailServiceApplication.java
@@ -8,50 +8,5 @@ public class GeoFenceDetailServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GeoFenceDetailServiceApplication.class, args);
-
-		/*MongoClient client = null;
-		try {
-
-			List<MongoCredential> credentials = new ArrayList<>();
-			credentials.add(MongoCredential.createScramSha1Credential("savioferns321", "savio_mongo", 
-					new char[] {'m','l','a','b','1','2','3'}));
-			ServerAddress address = new ServerAddress("ds041633.mlab.com:41633");
-			client = new MongoClient(address, credentials);
-			MongoDatabase database = client.getDatabase("admin");
-			MongoIterable<String> collections = database.listCollectionNames();
-			for (String string : collections) {
-				System.out.println(string);
-			}
-
-		} finally {
-			if(client != null)
-				client.close();
-		}*/
-
-		/*MongoClient client = new MongoClient(Arrays.asList(new ServerAddress("ds041633.mlab.com"), new ServerAddress("ds041633.mlab.com:41633")),
-				Collections.singletonList(MongoCredential.createCredential("savioferns321",
-						"admin",
-						"mlab123".toCharArray())),
-				MongoClientOptions.builder().serverSelectionTimeout(1000).build());
-
-
-		try {
-			client.getDB("admin").command("ping");
-		} catch (MongoTimeoutException e) {
-			// do something
-		}*/
-		
-		/*try {
-		    MongoClientURI mongoClientURI = new MongoClientURI("mongodb://savioferns321:mongodb123@ds041633.mlab.com:41633/savio_mongo");
-		    MongoClient mongoClient = new MongoClient(mongoClientURI);
-		    MongoDatabase database = mongoClient.getDatabase("savio_mongo");
-		    System.out.println(database.getCollection("addresses").count());
-		}
-		catch(Exception e) {
-		    // TODO: some proper exception handling
-		    System.err.println(e.getLocalizedMessage());
-		}
-*/
-
 	}
 }
